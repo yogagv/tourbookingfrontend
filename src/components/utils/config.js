e911f1287c8a0b607f46d40dabcf2ddcac9b3b1a
yogagv <yoganandhan465@gmail.com>
2024-08-12 11:31:22 +0530

    Testi commit

--- src/components/utils/config.js
+++ src/components/utils/config.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:3000/api/v1";
+// export const BASE_URL = "http://localhost:3000/api/v1";
 
-// export const BASE_URL = "https://tourbookingbackend-dquf.onrender.com";
+export const BASE_URL = "https://tourbookingbackend-dquf.onrender.com";
 
 export const token = localStorage.getItem("token");
